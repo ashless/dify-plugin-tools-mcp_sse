daa125406ab19f1d2b395aa48e1e3a6f96ed8a18
ashlesss <hidahza@gmail.com>
2025-04-15 02:28:08 -0400

    Refactor the tool call arguments parsing

--- tools/mcp_sse_call_tool.py
+++ tools/mcp_sse_call_tool.py
@@ -1,4 +1,4 @@
-import json
+import json, ast
 import logging
 from collections.abc import Generator
 from typing import Any
@@ -21,13 +21,32 @@ class McpSseTool(Tool):
             raise ValueError(f"servers_config must be a valid JSON string: {e}")
 
         tool_name = tool_parameters.get("tool_name", "")
+        print(f"tool_parameters: {tool_parameters}")
         if not tool_name:
             raise ValueError("Please fill in the tool_name")
         arguments_json = tool_parameters.get("arguments", "")
+        print(f"MCP_SSE_ARGS: {arguments_json}")
         if not arguments_json:
             raise ValueError("Please fill in the arguments")
         try:
-            arguments = json.loads(arguments_json)
+            """
+            Chatflow will return dict so that this wil cause json parse to failed
+
+            such as arguments_json will look like this 
+
+            {'limit': 5, 'order': 'random', 'sort': 'ascending'}
+
+            which is a str
+
+            directly feed this to json.loads will raise not double quote error
+
+            we should convert the str to dict and cast properties with double quote,
+            because all arguements pass to the tool are string
+            """
+            as_dict = ast.literal_eval(arguments_json)
+            arguments = {k: str(v) for k, v in as_dict.items()}
+
+            print(f"MCP_SSE_ARGS_JSON: {arguments}")
         except json.JSONDecodeError as e:
             raise ValueError(f"Arguments must be a valid JSON string: {e}")
 
